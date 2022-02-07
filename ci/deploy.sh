eca71a3617a7fd26d42f689c4cb6f080076dd333
Chiby <xaujade@gmail.com>
2022-02-07 11:13:53 +0100

    update deploy + docker-compose

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,9 +6,4 @@ docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
 
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 
-docker exec -t guestbook_guestbook_1 bash -c "symfony console doctrine:database:drop --force || true"
-docker exec -t guestbook_guestbook_1 bash -c "symfony console doctrine:database:create"
-docker exec -t guestbook_guestbook_1 bash -c "symfony console doctrine:migration:migrate -n"
-docker exec -t guestbook_guestbook_1 bash -c "APP_ENV=dev symfony console doctrine:fixtures:load -n"
-
 sh run.sh
